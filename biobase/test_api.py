86bb3301ef2005870ea19d7bd3224f58da8098c1
tofickbrodaga <mshdtlv@gmail.com>
2024-05-22 23:07:21 +0300

    Created StrainsApiTest, which work

--- biobase/test_api.py
+++ biobase/test_api.py
@@ -1,71 +1,93 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from biobaseapp.models import Strains
+from django.utils.timezone import now
 
-from biobaseapp.models import CustomUser, Strains
+CustomUser = get_user_model()
 
-def create_api_test(model_class, url, creation_attrs, user):
-    class ApiTest(TestCase):
-        @classmethod
-        def setUpClass(cls):
-            super().setUpClass()
-            # Create users
-            cls.user = CustomUser.objects.create_user(username='user123', password='user001')
-            cls.superuser = CustomUser.objects.create_superuser(username='admin123', password='admin01')
+class StrainsAPITest(APITestCase):
 
-            # Create tokens for users
-            cls.user_token = Token.objects.create(user=cls.user)
-            cls.superuser_token = Token.objects.create(user=cls.superuser)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = CustomUser.objects.create_user(username='user', password='user')
+        self.superuser = CustomUser.objects.create_superuser(username='admin', password='admin')
 
-        def setUp(self):
-            self.client = APIClient()
+        self.user_token = Token.objects.create(user=self.user)
+        self.superuser_token = Token.objects.create(user=self.superuser)
 
-        def api_methods(self, user, token, post_exp, put_exp, delete_exp):
-            self.client.force_authenticate(user=user, token=token)
+        self.url = '/api/strains/'
 
-            # Create model object
-            try:
-                created_obj = model_class.objects.create(**creation_attrs, created_by=user)
-                instance_url = f'{url}{created_obj.id}/'
-            except Exception as e:
-                print("Error creating object:", e)
-                raise
+        self.strain = Strains.objects.create(
+            UIN='N876',
+            name='Strain CD',
+            pedigree='sdfg',
+            mutations='dfg',
+            transformations='none',
+            creation_date=now().date(),
+            created_by=self.user
+        )
 
-            # Debugging
-            print("Created object:", created_obj)
-            print("Instance URL:", instance_url)
-            # GET all
-            self.assertEqual(self.client.options(url).status_code, status.HTTP_200_OK)
-            self.assertEqual(self.client.head(url).status_code, status.HTTP_200_OK)
-            self.assertEqual(self.client.get(url).status_code, status.HTTP_200_OK)
+    def api_methods(self, user: CustomUser, token: Token, post_exp: int, put_exp: int, delete_exp: int): # type: ignore
+        self.client.force_authenticate(user=user, token=token)
+
+        creation_attrs = {
+            'UIN': 'N877',
+            'name': 'New Strain',
+            'pedigree': 'xyz',
+            'mutations': 'abc',
+            'transformations': 'none',
+            'creation_date': now().date(),
+            'created_by': user.id
+        }
+
+        # POST
+        response = self.client.post(self.url, creation_attrs, format='json')
+        if response.status_code != post_exp:
+            print("POST response data:", response.data)
+        self.assertEqual(response.status_code, post_exp)
+        
+        if response.status_code == status.HTTP_201_CREATED:
+            self.created_id = response.data['id']
+            instance_url = f'{self.url}{self.created_id}/'
 
             # GET instance
-            self.assertEqual(self.client.get(instance_url).status_code, status.HTTP_200_OK)
-
-            # POST
-            self.assertEqual(self.client.post(url, creation_attrs).status_code, post_exp)
+            get_response = self.client.get(instance_url)
+            self.assertEqual(get_response.status_code, status.HTTP_200_OK)
 
             # PUT
-            self.assertEqual(self.client.put(instance_url, creation_attrs).status_code, put_exp)
+            put_response = self.client.put(instance_url, creation_attrs, format='json')
+            self.assertEqual(put_response.status_code, put_exp)
 
             # DELETE
-            self.assertEqual(self.client.delete(instance_url).status_code, delete_exp)
+            delete_response = self.client.delete(instance_url)
+            self.assertEqual(delete_response.status_code, delete_exp)
 
-        def test_superuser(self):
-            self.api_methods(
-                self.superuser, self.superuser_token,
-                status.HTTP_201_CREATED, status.HTTP_200_OK, status.HTTP_204_NO_CONTENT
-            )
+            # OPTIONS instance
+            options_response = self.client.options(instance_url)
+            self.assertEqual(options_response.status_code, status.HTTP_200_OK)
 
-        def test_user(self):
-            self.api_methods(
-                self.user, self.user_token,
-                status.HTTP_403_FORBIDDEN, status.HTTP_403_FORBIDDEN, status.HTTP_403_FORBIDDEN
-            )
+        # GET all
+        get_all_response = self.client.get(self.url)
+        self.assertEqual(get_all_response.status_code, status.HTTP_200_OK)
 
-    return ApiTest
+        # HEAD all
+        head_all_response = self.client.head(self.url)
+        self.assertEqual(head_all_response.status_code, status.HTTP_200_OK)
 
-# Example usage
-user = CustomUser.objects.get(username='md')  # Assuming 'admin123' is the username of the user who created the object
-StrainsApiTest = create_api_test(Strains, '/api/strains/', {'UIN': 'UIN1', 'name': 'Strain1', 'pedigree': 'Pedigree', 'mutations': 'Mutations', 'transformations': 'Transformations', 'creation_date': '2024-05-02'}, user)
+        # OPTIONS all
+        options_all_response = self.client.options(self.url)
+        self.assertEqual(options_all_response.status_code, status.HTTP_200_OK)
+
+    def test_superuser(self):
+        self.api_methods(
+            self.superuser, self.superuser_token,
+            status.HTTP_201_CREATED, status.HTTP_200_OK, status.HTTP_204_NO_CONTENT
+        )
+
+    def test_user(self):
+        self.api_methods(
+            self.user, self.user_token,
+            status.HTTP_403_FORBIDDEN, status.HTTP_403_FORBIDDEN, status.HTTP_403_FORBIDDEN
+        )
